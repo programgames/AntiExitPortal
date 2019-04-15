579f53ede260ccc64d6f8d88c59be1c3510a1fc8
Julien Marcon <julgamer1@gmail.com>
2019-04-15 21:22:05 +0200

    Upgrade dragon egg creation

--- src/main/java/com/programgames/antiexitportal/Main.java
+++ src/main/java/com/programgames/antiexitportal/Main.java
@@ -1,12 +1,9 @@
 package com.programgames.antiexitportal;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.PortalType;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,6 +13,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin implements Listener {
+
+  private FileConfiguration config = getConfig();
+
   @Override
   public void onDisable() {
     super.onDisable();
@@ -24,6 +24,8 @@ public class Main extends JavaPlugin implements Listener {
   @Override
   public void onEnable() {
     super.onEnable();
+    saveDefaultConfig();
+
     Bukkit.getServer().getPluginManager().registerEvents(this, this);
     Logger log = Bukkit.getLogger();
     log.info("Anti exit portal loaded");
@@ -32,11 +34,31 @@ public class Main extends JavaPlugin implements Listener {
 
   @EventHandler(priority = EventPriority.MONITOR)
   public void onEntityCreatePortal(EntityCreatePortalEvent event) {
-    Logger log = Bukkit.getLogger();
-    event.setCancelled(true);
-    log.info("Dragon killed");
-    event.getEntity().getLocation().getBlock().setType(Material.DRAGON_EGG);
 
+    boolean eggSpawned = false;
+    double y = 0;
+    if (event.getEntity() instanceof EnderDragon && event.getPortalType() == PortalType.ENDER) {
+      if (config.getBoolean("DragonKilled")) {
+        event.setCancelled(true);
+        World world = event.getEntity().getWorld();
+        Block above = world.getBlockAt(  (int) event.getEntity().getLocation().getX(),  (int) event.getEntity().getEyeHeight(), (int) event.getEntity().getLocation().getZ());
+        while(!eggSpawned) {
+          if (Material.AIR == above.getType()) {
+            above.setType(Material.DRAGON_EGG);
+            eggSpawned = true;
+          }
+          else {
+            above = world.getBlockAt(  (int) event.getEntity().getLocation().getX(),  (int) (event.getEntity().getEyeHeight() + y), (int) event.getEntity().getLocation().getZ());
+            y+=1;
+          }
+        }
+      } else {
+        this.getConfig().set("DragonKilled", true);
+        this.saveConfig();
+      }
+
+
+    }
   }
 }
 
